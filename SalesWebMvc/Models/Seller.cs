d32496465a7401434806e216e2f57792f26c2ecf
ltmartinelli <lucastrindademartinelli@gmail.com>
2022-06-30 15:53:51 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -10,27 +10,34 @@ namespace SalesWebMvc.Models
     {
         [Display(Name = "ID")]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1} characters")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
         [Display(Name = "E-Mail")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString="{0:dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
 
-        public Department Department { get; set; }
-
         [Display(Name = "Department")]
+        public Department Department { get; set; }
         public int DepartmentId { get; set; }
 
-		public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         //Constructors
 
